ac63e2daa9bfc394c7d4c7fe200fe5d4e6ae3248
emiliolj1 <lunaemilio269@gmail.com>
2026-06-19 19:09:32 -0300

    Creacion de carpeta Middleware

---
+++ Dsw2026Ej15.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Dsw2026Ej15.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Ej15.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = exception.Message
+            });
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var problem = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Ocurrió un error inesperado.",
+                Detail = "Intente nuevamente más tarde."
+            };
+
+            await context.Response.WriteAsJsonAsync(problem);
+        }
+    }
+}
