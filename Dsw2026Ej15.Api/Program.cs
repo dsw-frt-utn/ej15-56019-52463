80727c1ca78b4d95d9529916e00a1de5370e8ac1
emiliolj1 <lunaemilio269@gmail.com>
2026-06-19 19:40:30 -0300

    Implementacion de la api

--- Dsw2026Ej15.Api/Program.cs
+++ Dsw2026Ej15.Api/Program.cs
@@ -1,23 +1,32 @@
+using Dsw2026Ej15.Api.Middlewares;
+using Dsw2026Ej15.Data;
+using Dsw2026Ej15.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddSingleton<IPersistence, PersistenceInMemory>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.MapHealthChecks("/health-check");
 
 app.Run();
